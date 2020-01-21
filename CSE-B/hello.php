72b967c02bfff12b32c815d1346dbde57bdb9a3c
anushkasaxena <anushka.saxena.68@gmail.com>
2020-01-21 11:39:19 +0530

    upload hello.php

--- CSE-B/hello.php
+++ CSE-B/hello.php
@@ -1,3 +1,22 @@
-<?php
-echo ("<h1>Welcome to Graphic Era!!</h1>");
-?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>inout details</title>
+	
+</head>
+<body>
+	<h1>Welcome </h1>
+	first_name: <input id='first' type="text" placeholder="enter first name"><br>
+	second_name: <input id='second' type="text" placeholder="enter second name"><br>
+	<input id='btn' type="submit" placeholder="submit">
+	<script type="text/javascript">
+		document.getElementById('btn').onclick = function()
+		{
+			first_name=document.getElementById('first').value;
+			second_name=document.getElementById('second').value;
+			console.log(first_name+" "+second_name);
+		}
+	</script>
+</body>
+</html>
